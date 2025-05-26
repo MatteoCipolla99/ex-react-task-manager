8be0428b936ad70382d9c3f72517f02d16571ce0
Matteo Cipolla <mati_teo@live.it>
2025-05-26 12:09:28 +0200

    add-milestone-10

---
+++ ex-react-task-manager/src/components/EditTaskModal.jsx
@@ -0,0 +1,64 @@
+import { useState, useRef } from "react";
+import Modal from "./Modal";
+
+export default function EditTaskModal({ show, onClose, task, onSave }) {
+  const [editedTask, setEditedtask] = useState(task);
+
+  const editFormRef = useRef();
+
+  const changeEditedTask = (key, event) => {
+    setEditedtask((prev) => ({
+      ...prev,
+      [key]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSave(editedTask);
+  };
+
+  const { title, description, status } = editedTask;
+
+  return (
+    <Modal
+      title="Modifica Task"
+      content={
+        <form ref={editFormRef} onSubmit={handleSubmit}>
+          <label>
+            Nome Task :
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => changeEditedTask("title", e)}
+            />
+          </label>
+          <label>
+            Descrizione:
+            <textarea
+              value={description}
+              onChange={(e) => changeEditedTask("description", e)}
+            />
+          </label>
+          <label>
+            Stato:
+            <select
+              value={status}
+              onChange={(e) => changeEditedTask("status", e)}
+            >
+              {["To do", "Doing", "Done"].map((value, index) => (
+                <option key={index} value={value}>
+                  {value}
+                </option>
+              ))}
+            </select>
+          </label>
+        </form>
+      }
+      confirmText="Salva"
+      show={show}
+      onClose={onClose}
+      onConfirm={() => editFormRef.current.requestSubmit()}
+    />
+  );
+}
--- ex-react-task-manager/src/pages/TaskDetail.jsx
+++ ex-react-task-manager/src/pages/TaskDetail.jsx
@@ -2,15 +2,17 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 import Modal from "../components/Modal";
+import EditTaskModal from "../components/EditTaskModal";
 
 export default function TaskDetail() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const { tasks, removeTask } = useContext(GlobalContext);
+  const { tasks, removeTask, updateTask } = useContext(GlobalContext);
 
   const task = tasks.find((task) => task.id === parseInt(id));
 
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
 
   if (!task) {
     return <h2>Task non trovata</h2>;
@@ -21,6 +23,16 @@ export default function TaskDetail() {
       await removeTask(task.id);
       alert("Task eliminato con successo");
       navigate("/");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+
+  const handleUpdate = async (updatedTask) => {
+    try {
+      await updateTask(updatedTask);
+      setShowEditModal(false);
     } catch (error) {
       console.error(error);
       alert(error.message);
@@ -47,6 +59,8 @@ export default function TaskDetail() {
         {new Date(task.createdAt).toLocaleDateString()}
       </p>
       <button onClick={() => setShowDeleteModal(true)}>Elimina Task</button>
+      <button onClick={() => setShowEditModal(true)}>Modifica Task</button>
+
       <Modal
         title="Conferma Eliminazione"
         content={<p>Confermi di eliminare la task</p>}
@@ -54,6 +68,13 @@ export default function TaskDetail() {
         onClose={() => setShowDeleteModal(false)}
         onConfirm={handleDelete}
         confirmText="Elimina"
+      />
+
+      <EditTaskModal
+        task={task}
+        show={showEditModal}
+        onClose={() => setShowEditModal(false)}
+        onSave={handleUpdate}
       />
     </div>
   );
